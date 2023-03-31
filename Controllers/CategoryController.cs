a442bc3f067a9fd36f00d7b151fdcc96a228b2b8
Luís Felipe <luisrodrigues@bne.com.br>
2023-03-31 09:40:31 -0300

    Aulas 08 a 14 - Rotas REST, Tasks, ActionResults e Validações dos Models

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoH_WebApi_DD_7194.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjetoH_WebApi_DD_7194.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get()
+        {
+            return new List<Category>();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById(int id)
+        {
+            return new Category();
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post([FromBody] Category model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(model);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Category model)
+        {
+            if (model.Id != id)
+                return NotFound(new { message = "Categoria não encontrada"} );
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public async Task<ActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -16,5 +16,7 @@ namespace ProjetoH_WebApi_DD_7194.Models
         [MaxLength(20, ErrorMessage = "Este campo deve conter de 3 a 20 caracteres!")]
         [MinLength(3, ErrorMessage = "Este campo deve conter de 3 a 20 caracteres!")]
         public string Password { get; set; }
+
+        public string Role { get; set; }
     }
 }
